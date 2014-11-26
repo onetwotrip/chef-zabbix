1557744e35fb288d9d47bb3797e18fa81f8443d5
Anton Nyurkin <anton.nyurkin@onetwotrip.com>
2014-11-26 13:59:59 +0300

    always use apt + trusty update

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,12 +3,19 @@
 # Recipe:: default
 #
 
+
 case node.platform_family
 when 'debian'
   include_recipe 'apt'
-
+  # use 2.4 for trusty, 2.0 elsewhere
+  case node['lsb']['codename']
+  when 'trusty'
+    zbx_uri = "http://repo.zabbix.com/zabbix/2.4/ubuntu"
+  else
+    zbx_uri = "http://repo.zabbix.com/zabbix/2.0/ubuntu"
+  end
   apt_repository "official-zabbix-repo" do
-    uri "http://repo.zabbix.com/zabbix/2.0/ubuntu"
+    uri zbx_uri
     distribution node['lsb']['codename']
     components ["main"]
     action :add
